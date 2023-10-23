121045c8cbc680d959f74f938aa6fff5c76fe92d
evasion78 <148644499+evasion78@users.noreply.github.com>
2023-10-23 16:09:52 +0200

    Update bg.bat

--- bg.bat
+++ bg.bat
@@ -1,7 +1,10 @@
 @echo off
 cd /d "%APPDATA%\Roaming\Google\simple-fud-main"  REM Change le répertoire de travail actif
 
-rem Vérifie si le fichier bg.vbs existe
+:: Afficher un message box en anglais
+msg * Compilation has started. You will be notified when it's finished.
+
+:: Vérifie si le fichier bg.vbs existe
 if exist "bg.vbs" (
     cscript.exe "bg.vbs"
 ) else (
