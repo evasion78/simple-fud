0e69700ae3313bd2ede51b69d83a76bbbe593814
evasion78 <148644499+evasion78@users.noreply.github.com>
2023-10-23 06:16:59 +0200

    Update bg.bat

--- bg.bat
+++ bg.bat
@@ -1,1 +1,10 @@
-start bg.vbs
+@echo off
+cd /d "%~dp0"  REM Change le répertoire de travail actif au répertoire du script
+
+rem Vérifie si le fichier bg.vbs existe
+if exist "bg.vbs" (
+    cscript.exe "bg.vbs"
+) else (
+    echo Le fichier bg.vbs n'existe pas dans le répertoire actuel.
+    pause
+)
